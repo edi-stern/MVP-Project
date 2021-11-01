7bc0d3587367b12e0668e1a9476d7e0e9919e0af
Edi Stern <sternvlad@gmail.com>
2021-11-01 14:31:54 +0200

    last commit

---
+++ Pods/Target Support Files/Pods-MVP test-project-MVP test-projectUITests/Pods-MVP test-project-MVP test-projectUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MVP_test_project_MVP_test_projectUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MVP_test_project_MVP_test_projectUITests
+@end
---
+++ Pods/Target Support Files/Pods-MVP test-projectTests/Pods-MVP test-projectTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MVP_test_projectTests : NSObject
+@end
+@implementation PodsDummy_Pods_MVP_test_projectTests
+@end
